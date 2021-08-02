34bf09983521e7cd80613ca28a1855923241e3e3
Miroslav Zarenkov <miroslav@MacBook-Pro-Miroslav.local>
2021-08-02 19:58:04 +0300

    Add change grid size functionality

--- script.js
+++ script.js
@@ -22,44 +22,40 @@ for (let i=0; i<fieldSize; i++){
     gameContainer.appendChild(gamePixelsDiv);
 }
 
+
+colorPixels();
+
+function changeFieldSizeText(){
+    gameContainer.innerHTML='';
+    fieldSizeTextP = document.querySelector('#field-size-text');
+    fieldSizeText = document.querySelector('#field-size').value;
+    fieldSizeTextP.textContent = fieldSizeText;
+    columnsNumber = fieldSizeText;
+    rowsNumber = fieldSizeText;
+    fieldSize = rowsNumber * columnsNumber;
+    let cssChange = 'repeat('+rowsNumber+', 1fr)';
+    document.querySelector('.game').style.gridTemplateColumns = cssChange;
+    document.querySelector('.game').style.gridTemplateRows = cssChange;
+    for (let i=0; i<fieldSize; i++){
+        gamePixelsDiv = document.createElement('div');
+        gamePixelsDiv.setAttribute('class', 'game-pixels');
+        gameContainer.appendChild(gamePixelsDiv);
+    };
+    colorPixels();
+}
+   
+function colorPixels(){
 let gamePixels = document.querySelectorAll('.game-pixels');
 gamePixels.forEach(item => {
     item.addEventListener('mouseover', () => {
     item.classList.add('hover-color');
   })
 });
+}
 
 function removeColor(){
+    let gamePixels = document.querySelectorAll('.game-pixels');
     gamePixels.forEach(item => {
         item.classList.remove('hover-color');
       });
 }
-
-//up works
-//bellow responsive grid test
-function changeFieldSizeText(){
-    fieldSizeText = document.querySelector('#field-size').value;
-    console.log(fieldSizeText);
-    fieldSizeTextP.textContent = fieldSizeText;
-    console.log(fieldSizeTextP);
-    columnsNumber = fieldSizeText;
-    rowsNumber = fieldSizeText;
-    console.log("changed columns " + columnsNumber,"changed rows " + rowsNumber);
-    fieldSize = rowsNumber * columnsNumber;
-    console.log("changed field size " + fieldSize);
-    //smth below not working
-    document.querySelector('.game').style.gridTemplateColumns = 'repeat(16, 1fr)';
-    document.querySelector('.game').style.gridTemplateRows = 'repeat(16, 1fr)';
-    //document.querySelector('.game').style.gridTemplateRows = '32, 1fr';
-    gameContainer.removeChild(gamePixelsDiv);
-    for (let i=0; i<fieldSize; i++){
-        let gamePixelsDiv = document.createElement('div');
-        gamePixelsDiv.setAttribute('class', 'game-pixels');
-        gameContainer.appendChild(gamePixelsDiv);
-    }
-    
-    
-}
-//below works
-
-//ДОДЕЛАТЬ РЕСПОНСИВ СЕТКУ ГРИДА при изменении размера сетки нужно удалить старые div и создать сетку с новыми div
