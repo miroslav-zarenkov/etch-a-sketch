0e9e08a4607b8c2e0bfc7dbc85216fecc6c15737
Miroslav Zarenkov <miroslav@MBP-Miroslav.Dlink>
2021-08-02 16:50:04 +0300

    Change from Flexbox to Grid. Add dynamic rows prototype.

--- script.js
+++ script.js
@@ -1,24 +1,35 @@
 let gameContainer = document.querySelector('.game')
 
-for (let i=0; i<16; i++){
-    let gamePixels = document.createElement('div');
-    gamePixels.setAttribute('class', 'game-pixels');
-    gameContainer.appendChild(gamePixels);
+
+let clearBtn = document.querySelector('#clear-button');
+clearBtn.addEventListener('click', removeColor);
+
+let htmlStyles = window.getComputedStyle(document.querySelector("html"));
+let rowsNumber = parseInt(htmlStyles.getPropertyValue("--rowsNumber"));
+let columnsNumber = parseInt(htmlStyles.getPropertyValue("--columnsNumber"));
+console.log(rowsNumber, columnsNumber);
+let fieldSize = rowsNumber * columnsNumber;
+console.log(fieldSize);
+
+
+
+
+for (let i=0; i<fieldSize; i++){
+    let gamePixelsDiv = document.createElement('div');
+    gamePixelsDiv.setAttribute('class', 'game-pixels');
+    gameContainer.appendChild(gamePixelsDiv);
 }
 
 let gamePixels = document.querySelectorAll('.game-pixels');
-
 gamePixels.forEach(item => {
     item.addEventListener('mouseover', () => {
     item.classList.add('hover-color');
   })
 });
 
-let clearBtn = document.querySelector('#clear-button');
-clearBtn.addEventListener('click', removeColor);
-
 function removeColor(){
     gamePixels.forEach(item => {
         item.classList.remove('hover-color');
       });
 }
+
