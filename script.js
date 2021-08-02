df6af3c8871e674156fb3a8f98891ba39cd635b8
Miroslav Zarenkov <miroslav@MBP-Miroslav.Dlink>
2021-08-02 18:15:28 +0300

    Attempt to create responsive grid failed

--- script.js
+++ script.js
@@ -4,18 +4,20 @@ let gameContainer = document.querySelector('.game')
 let clearBtn = document.querySelector('#clear-button');
 clearBtn.addEventListener('click', removeColor);
 
+let fieldSizeText = document.querySelector('#field-size').value;
 let htmlStyles = window.getComputedStyle(document.querySelector("html"));
-let rowsNumber = parseInt(htmlStyles.getPropertyValue("--rowsNumber"));
-let columnsNumber = parseInt(htmlStyles.getPropertyValue("--columnsNumber"));
-console.log(rowsNumber, columnsNumber);
+let rowsNumber = parseInt(fieldSizeText);
+let columnsNumber = parseInt(fieldSizeText);
 let fieldSize = rowsNumber * columnsNumber;
-console.log(fieldSize);
 
 
+
+let fieldSizeTextP = document.querySelector('#field-size-text');
+fieldSizeTextP.textContent = fieldSizeText;
 
 
 for (let i=0; i<fieldSize; i++){
-    let gamePixelsDiv = document.createElement('div');
+    gamePixelsDiv = document.createElement('div');
     gamePixelsDiv.setAttribute('class', 'game-pixels');
     gameContainer.appendChild(gamePixelsDiv);
 }
@@ -33,3 +35,31 @@ function removeColor(){
       });
 }
 
+//up works
+//bellow responsive grid test
+function changeFieldSizeText(){
+    fieldSizeText = document.querySelector('#field-size').value;
+    console.log(fieldSizeText);
+    fieldSizeTextP.textContent = fieldSizeText;
+    console.log(fieldSizeTextP);
+    columnsNumber = fieldSizeText;
+    rowsNumber = fieldSizeText;
+    console.log("changed columns " + columnsNumber,"changed rows " + rowsNumber);
+    fieldSize = rowsNumber * columnsNumber;
+    console.log("changed field size " + fieldSize);
+    //smth below not working
+    document.querySelector('.game').style.gridTemplateColumns = 'repeat(16, 1fr)';
+    document.querySelector('.game').style.gridTemplateRows = 'repeat(16, 1fr)';
+    //document.querySelector('.game').style.gridTemplateRows = '32, 1fr';
+    gameContainer.removeChild(gamePixelsDiv);
+    for (let i=0; i<fieldSize; i++){
+        let gamePixelsDiv = document.createElement('div');
+        gamePixelsDiv.setAttribute('class', 'game-pixels');
+        gameContainer.appendChild(gamePixelsDiv);
+    }
+    
+    
+}
+//below works
+
+//ДОДЕЛАТЬ РЕСПОНСИВ СЕТКУ ГРИДА при изменении размера сетки нужно удалить старые div и создать сетку с новыми div
