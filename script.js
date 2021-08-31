ee9c8a6eb4594accf6bf2876344ce135cd377379
Miroslav Zarenkov <miroslav@MBP-Miroslav.Dlink>
2021-08-31 23:22:39 +0300

    Minor fixes to code

--- script.js
+++ script.js
@@ -12,20 +12,31 @@ let rowsNumber = parseInt(fieldSizeText);
 let columnsNumber = parseInt(fieldSizeText);
 let fieldSize = rowsNumber * columnsNumber;
 
-
-
 let fieldSizeTextP = document.querySelector('#field-size-text');
 fieldSizeTextP.textContent = "Field size: " + fieldSizeText + "x" + fieldSizeText;
 
+let fieldSizeRange = document.querySelector('#field-size');
+fieldSizeRange.addEventListener('change', changeFieldSizeText);
 
+function createDivs(){
 for (let i=0; i<fieldSize; i++){
     gamePixelsDiv = document.createElement('div');
     gamePixelsDiv.setAttribute('class', 'game-pixels');
     gameContainer.appendChild(gamePixelsDiv);
 }
+}
+
+createDivs();
+
+function removeLastChildDiv(){
+    let myNode = document.querySelector(".game");
+    while (myNode.firstChild) {
+      myNode.removeChild(myNode.lastChild);
+}
+}
 
 function changeFieldSizeText(){
-    gameContainer.innerHTML='';
+    removeLastChildDiv();
     fieldSizeTextP = document.querySelector('#field-size-text');
     fieldSizeText = document.querySelector('#field-size').value;
     fieldSizeTextP.textContent = "Field size: " + fieldSizeText + "x" + fieldSizeText;
@@ -35,11 +46,7 @@ function changeFieldSizeText(){
     let cssChange = 'repeat('+rowsNumber+', 1fr)';
     document.querySelector('.game').style.gridTemplateColumns = cssChange;
     document.querySelector('.game').style.gridTemplateRows = cssChange;
-    for (let i=0; i<fieldSize; i++){
-        gamePixelsDiv = document.createElement('div');
-        gamePixelsDiv.setAttribute('class', 'game-pixels');
-        gameContainer.appendChild(gamePixelsDiv);
-    };
+    createDivs();
     colorPixels();
 }
    
@@ -48,7 +55,6 @@ let gamePixels = document.querySelectorAll('.game-pixels');
 gamePixels.forEach(item => {
     item.addEventListener('mouseover', () => {
     item.style.backgroundColor = color;
-        //item.classList.add('hover-color');
   })
 });
 }
@@ -56,17 +62,12 @@ gamePixels.forEach(item => {
 function removeColor(){
     let gamePixels = document.querySelectorAll('.game-pixels');
     gamePixels.forEach(item => {
-        //item.classList.remove('hover-color');
         item.style.backgroundColor = 'white';
       });
 }
 
 function changeColor(){
     color = document.querySelector('#pixel-color').value;
-    //document.querySelector('.hover-color').style.backgroundColor = color;
-    //console.log(color);
-    
 }
-
 
 colorPixels();
